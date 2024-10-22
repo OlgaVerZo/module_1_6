cbbb95e0af05c6a5a496f4233a6926193a58ff96
OlgaVerZo <verkienko@gmail.com>
2024-10-22 13:59:57 +0400

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+dict = {k : sum(v)/len(v) for k, v in zip(sorted(students), grades)}
+print(dict)
+
